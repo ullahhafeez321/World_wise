0b24e2d696540b5a4ea06d25ce28f33795550048
Hafeez Ullah <ullahhafeez321@gmail.com>
2026-01-13 23:15:17 +0500

    changed again the files

--- dist/assets/Product.module-Be8LLB42.js
+++
@@ -1,1 +0,0 @@
-const t="_product_1ijzm_1",o={product:t};export{o as s};
--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import { lazy, Suspense } from "react";
-import { HashRouter, Navigate, Route, Routes } from "react-router-dom";
-// import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 
 import { CitiesProvider } from "./contexts/CitiesContext";
 import { AuthProvider } from "./contexts/FakeAuthContext";
@@ -31,7 +30,7 @@ export default function App() {
     <div>
       <AuthProvider>
         <CitiesProvider>
-          <HashRouter>
+          <BrowserRouter>
             <Suspense fallback={<SpinnerFullPage />}>
               <Routes>
                 <Route index element={<HomePage />} />
@@ -55,7 +54,7 @@ export default function App() {
                 <Route path="*" element={<PageNotFound />} />
               </Routes>
             </Suspense>
-          </HashRouter>
+          </BrowserRouter>
         </CitiesProvider>
       </AuthProvider>
     </div>
